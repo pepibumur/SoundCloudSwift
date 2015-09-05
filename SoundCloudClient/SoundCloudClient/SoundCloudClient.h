49b7e55a080d8de0ae8f3052b182ae8093dda214
Pepi <pepibumur@gmail.com>
2015-09-05 10:39:22 +0200

    Created project

---
+++ SoundCloudClient/SoundCloudClient/SoundCloudClient.h
@@ -0,0 +1,19 @@
+//
+//  SoundCloudClient.h
+//  SoundCloudClient
+//
+//  Created by Pedro Piñera Buendía on 05/09/15.
+//  Copyright © 2015 ppinera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SoundCloudClient.
+FOUNDATION_EXPORT double SoundCloudClientVersionNumber;
+
+//! Project version string for SoundCloudClient.
+FOUNDATION_EXPORT const unsigned char SoundCloudClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SoundCloudClient/PublicHeader.h>
+
+
